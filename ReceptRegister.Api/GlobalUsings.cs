ca1f918cd89e8519a13180b43312559a61d9e50d
stefa <stefa@DESKTOP-JFUT92A>
2025-08-31 12:56:29 +0200

    refactor(api): convert API project to class library (single host)

---
+++ ReceptRegister.Api/GlobalUsings.cs
@@ -0,0 +1,12 @@
+// Central global usings for ASP.NET Core types that were implicitly provided by the Web SDK.
+// After converting the API project to a class library (Microsoft.NET.Sdk), we add them explicitly.
+
+global using Microsoft.AspNetCore.Builder;
+global using Microsoft.AspNetCore.Http;
+global using Microsoft.AspNetCore.Routing;
+global using Microsoft.AspNetCore.Localization;
+global using Microsoft.AspNetCore.Hosting;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Logging;
+global using Microsoft.Extensions.Hosting;
